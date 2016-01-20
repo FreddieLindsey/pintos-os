12d27f8e1580d3ecf538db77fac36d750733ca0c
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-01-20 17:03:24 +0000

    Update shutdown.c

--- src/devices/shutdown.c
+++ src/devices/shutdown.c
@@ -101,7 +101,7 @@ shutdown_power_off (void)
 
   /* This is a special power-off sequence supported by Bochs and
      QEMU, but not by physical hardware. */
-  outw( 0x604, 0x0 | 0x2000 );
+  outw (0xB004, 0x2000);
 
   /* This will power off a VMware VM if "gui.exitOnCLIHLT = TRUE"
      is set in its configuration file.  (The "pintos" script does
