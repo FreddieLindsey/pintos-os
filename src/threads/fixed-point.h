53144dc746cfe8f7b5659ea7a91e0b825ef90c43
Oliver Norton <otn14@imperial.ac.uk>
2016-01-28 15:11:14 +0000

    Added all of the functions. TODO test

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -10,9 +10,13 @@
                              else  \
                                 (((x) - (f)) /2)
 #define AddFPFP(x, y) ((x) + (y))
+#define AddFPInt(x, n, f) ((x) + ((n)*(f)))
 #define SubFPFP(x, y) ((x) - (y))
+#define SubFPInt(x, n, f) ((x) - ((n)*(f)))
 #define MultiplyFPInt(x, n) ((x) * (n))
+#define MultiplyFPFP(x, y, f) ((((int64_t)(x)) * (y))/(f))
 #define DivideFPInt(x, n) ((x) / (n))
+#define DivideFPFP(x, y, f) ((((int64_t)(x)) * (f))/(y))
 
 #endif
 
