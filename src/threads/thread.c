584d956f6eaab5c0ed2ae250bc33c7921048107c
James Lane <jrl14@imperial.ac.uk>
2016-02-08 14:37:05 +0000

    Fixed wrapping lines

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -256,7 +256,8 @@ thread_unblock (struct thread *t)
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
   // Synchronisation safe -> List becomes ordered based on priority
-  list_insert_ordered(&ready_list, &t->elem, (list_less_func*) thread_compare, NULL);
+  list_insert_ordered(&ready_list, &t->elem, 
+      (list_less_func*) thread_compare, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 
@@ -347,7 +348,8 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread)
-    list_insert_ordered(&ready_list, &cur->elem, (list_less_func*) thread_compare, NULL);
+    list_insert_ordered(&ready_list, &cur->elem, 
+        (list_less_func*) thread_compare, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -398,7 +400,8 @@ int thread_get_priority_of(struct thread *t) {
     return t->priority;
   } else {
     struct list_elem *front = list_front(&t->priorities);
-    return max(t->priority, list_entry(front, struct priority_elem, elem)->priority);
+    return max(t->priority, 
+        list_entry(front, struct priority_elem, elem)->priority);
   }
 }
 
@@ -560,8 +563,10 @@ void thread_add_priority(struct thread *t, int priority, struct lock *lock) {
   p->t = t;
   *d = *p;
   t->donated = true;
-  list_insert_ordered(&t->priorities, &p->elem, (list_less_func*) priority_compare, NULL);
-  list_insert_ordered(&thread_current()->donations, &d->elem, (list_less_func*) priority_compare, NULL);
+  list_insert_ordered(&t->priorities, &p->elem, 
+      (list_less_func*) priority_compare, NULL);
+  list_insert_ordered(&thread_current()->donations, &d->elem, 
+      (list_less_func*) priority_compare, NULL);
 
 }
 
