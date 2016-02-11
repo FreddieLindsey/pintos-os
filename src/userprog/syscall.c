8a3a1dd6f9b6cbed6d91956e0c41f5286813a17d
Oliver Norton <otn14@imperial.ac.uk>
2016-02-11 15:10:31 +0000

    Created prototypes of the system calls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -18,3 +18,55 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
   thread_exit ();
 }
+
+void halt (void) {
+
+}
+
+void exit (int status UNUSED) {
+
+}
+
+int exec (const char *file UNUSED) {
+  return 0;
+}
+
+int wait (int pid UNUSED) {
+  return 0;
+}
+
+bool create (const char *file UNUSED, unsigned initial_size UNUSED) {
+  return false;
+}
+
+bool remove (const char *file UNUSED) {
+  return false;
+}
+
+int open (const char *file UNUSED) {
+  return 0;
+}
+
+int filesize (int fd UNUSED) {
+  return 0;
+}
+
+int read (int fd UNUSED, void *buffer UNUSED, unsigned length UNUSED) {
+  return 0;
+}
+
+int write (int fd UNUSED, const void *buffer UNUSED, unsigned length UNUSED) {
+  return 0;
+}
+
+void seek (int fd UNUSED, unsigned position UNUSED) {
+
+}
+
+unsigned tell (int fd UNUSED) {
+  return 0;
+}
+
+void close (int fd UNUSED) {
+
+}
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,26 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <stdbool.h>
+
+
 void syscall_init (void);
+
+/* pid_t should be defined appropriately (for exec and wait) */
+
+/* Tasks 2 and later system calls. */
+void halt (void);
+void exit (int status);
+int exec (const char *file);
+int wait (int);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned length);
+int write (int fd, const void *buffer, unsigned length);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
 
 #endif /* userprog/syscall.h */
