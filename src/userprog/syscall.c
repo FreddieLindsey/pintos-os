2afb9fc5a28af51e45fb2e944e85c1b4dde1a3ac
Oliver Norton <otn14@imperial.ac.uk>
2016-02-12 12:38:23 +0000

    Implemented syscall handler but may need to be tidied up

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,7 +4,10 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+#define MAX_ARGS 3
+
 static void syscall_handler (struct intr_frame *);
+void read_args(void* esp, int num, void** args);
 
 void
 syscall_init (void)
@@ -13,10 +16,59 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED)
+syscall_handler (struct intr_frame *f)
 {
+
+  /* Read the number of the system call */
+  int syscall_num = *(int*)(f->esp);
+
+  /* Read the arguments of the system call */
+
+  /* array which holds the arguments of the system call */
+  /* also passes to the appropriate function */
+  void* args[MAX_ARGS];
+  switch (syscall_num) {
+    case SYS_HALT: halt(); break;
+    case SYS_EXIT: read_args(f->esp, 1, args);
+                   exit(*(int*)args[0]); break;
+    case SYS_EXEC: read_args(f->esp, 1, args);
+                   f->eax = exec((char*)args[0]); break;
+    case SYS_WAIT: read_args(f->esp, 1, args);
+                   f->eax = wait(*(int*)args[0]); break;
+    case SYS_CREATE: read_args(f->esp, 2, args);
+                     f->eax = create((char*)args[0], *(unsigned*)args[1]); break;
+    case SYS_REMOVE: read_args(f->esp, 1, args);
+                     f->eax = remove((char*)args[0]); break;
+    case SYS_OPEN: read_args(f->esp, 1, args);
+                     f->eax = open((char*)args[0]); break;
+    case SYS_FILESIZE: read_args(f->esp, 1, args);
+                       f->eax = filesize(*(int*)args[0]); break;
+    case SYS_READ: read_args(f->esp, 3, args);
+                   f->eax = read(*(int*)args[0], args[1], *(unsigned*)args[2]);
+                   break;
+    case SYS_WRITE: read_args(f->esp, 3, args);
+                    f->eax = write(*(int*)args[0], args[1], *(unsigned*)args[2]);
+                    break;
+    case SYS_SEEK: read_args(f->esp, 2, args);
+                   seek(*(int*)args[0], *(unsigned*)args[1]); break;
+    case SYS_TELL: read_args(f->esp, 1, args);
+                   f->eax = tell(*(int*)args[0]); break;
+    case SYS_CLOSE: read_args(f->esp, 1, args);
+                    close (*(int*)args[0]); break;
+  }
+
   printf ("system call!\n");
   thread_exit ();
+}
+
+/* Reads num arguments from esp and stores them in args */
+void read_args(void* esp, int num, void** args) {
+  int i = 0;
+  for (; i < num; i++) {
+    esp++;
+    args[i] = esp;
+  }
+
 }
 
 void halt (void) {
