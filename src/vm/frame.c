75c7b24ac052462d555f57c5307f822cf2a50013
Oliver Norton <otn14@imperial.ac.uk>
2016-03-08 17:32:49 +0000

    Changed frame to struct.
    Signed-off-by: Oliver Norton <otn14@imperial.ac.uk>

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -2,10 +2,10 @@
 #include "threads/malloc.h"
 
 
-static void** frame_table;
+static struct frame* frame_table;
 
 void frame_init(int num_of_frames) {
 
-  frame_table = malloc(sizeof(void*)*num_of_frames);
+  frame_table = malloc(sizeof(struct frame)*num_of_frames);
 
 }
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,6 +1,13 @@
 #ifndef FRAME_H
 #define FRAME_H
 
+#include "userprog/process.h"
+
+struct frame {
+  void* page;  /* pointer to user page */
+  pid_t pid;   /* id of the process that owns the frame */
+};
+
 void frame_init(int num_of_frames);
 
 
