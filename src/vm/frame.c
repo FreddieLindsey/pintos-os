6a775c26f1d63922b78e0f70bcdf671e92abf5b8
Oliver Norton <otn14@imperial.ac.uk>
2016-03-09 12:52:14 +0000

    Added synchronisation to frame-table
    Signed-off-by: Oliver Norton <otn14@imperial.ac.uk>

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -4,43 +4,46 @@
 
 
 static struct frame **frame_table;
+
+static struct lock frame_table_lock; /* lock for mutual exclusion */
+
 static size_t num_frames;
 
 void frame_init(int num_of_frames) {
 
   num_frames = num_of_frames;
   frame_table = malloc(sizeof(struct frame*)*num_frames);
+  lock_init(&frame_table_lock);
 
 }
 
 void frame_alloc(void* page) {
   unsigned f;
-  int allocated = 0;
   for(f = 0; f < num_frames; f++) {
+    lock_acquire(&frame_table_lock);
     if (frame_table[f] == NULL) {
-      allocated = 1;
       struct frame *frame = malloc(sizeof(struct frame));
       frame->page = page;
       frame->pid = thread_current()->tid;
       frame_table[f] = frame;
+      lock_release(&frame_table_lock);
+      return;
     }
+    lock_release(&frame_table_lock);
   }
-
-  if (!allocated) {
-    exit(-1);
-  }
+  exit(-1);
 }
 
 void frame_free(void* page) {
   unsigned f;
-  int allocated = 0;
   for(f = 0; f < num_frames; f++) {
+    lock_acquire(&frame_table_lock);
     if (frame_table[f] != NULL && frame_table[f]->page == page) {
       free(frame_table[f]);
+      lock_release(&frame_table_lock);
+      return;
     }
+    lock_release(&frame_table_lock);
   }
 
-  if (!allocated) {
-    exit(-1);
-  }
 }
