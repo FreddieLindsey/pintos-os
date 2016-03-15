674ee9f1ddf06b1796d4a0c23a3c408dec42213c
eb1314 <eb1314@ic.ac.uk>
2016-03-15 13:29:54 +0000

    working on the swap table

---
+++ src/vm/swap.h
@@ -0,0 +1,16 @@
+#ifndef SWAP_H
+#define SWAP_H
+
+#include "devices/block.h"
+
+struct swap {
+  struct page *page;       /* evicted page */
+  size_t slot;             /* swap slot */
+  struct list_elem elem;  /* needed for list */
+};
+
+void swap_init(void);
+void swap_alloc(struct page *page);
+void swap_free(struct page *page);
+
+#endif
